b14ecf1a5923d86ef72fd665474060de1eed0d87
Gloria Anderson <hamman.gja@gmail.com>
2022-03-25 12:00:32 -0400

    added imports needed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
+//importing hook from React
+import { useState, useEffect } from "react";
+
+//import Chat Box component
+import ChatBox from "./components/ChatBox.jsx"
+
+//style import
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <h1>Hello, Chat!</h1>
