6a8dca9d4130dfdd0e7b33801fa204eadef02dbd
Gloria Anderson <hamman.gja@gmail.com>
2022-03-26 18:13:00 -0400

    made nav into list

--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -2,16 +2,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+//Style Imports
+import "../styles/App.css";
+
 //Routing funtion to other rooms in chat app
 export default function RoomLinks() {
   return (
     <section class="nav-box">
       <h3>Available Rooms</h3>
       <nav>
-        <NavLink to="/general">General</NavLink>
-        <NavLink to="/pets">Pets</NavLink>
-        <NavLink to="/food">Food</NavLink>
-        <NavLink to="/Books">Books</NavLink>
+        <ul>
+          <li>
+            <NavLink to="/general">General</NavLink>
+          </li>
+          <li><NavLink to="/pets">Pets</NavLink></li>
+          <li><NavLink to="/food">Food</NavLink></li>
+          <li><NavLink to="/Books">Books</NavLink></li>
+        </ul>
       </nav>
     </section>
   );
