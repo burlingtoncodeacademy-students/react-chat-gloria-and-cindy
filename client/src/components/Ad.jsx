c1b91513ef9c0719fc4301340e11a542c66bed90
Cindy Marcelle <88116236+cmarcelle@users.noreply.github.com>
2022-03-27 13:43:05 -0400

    Update Ad.jsx
    updated main div with class id

--- client/src/components/Ad.jsx
+++ client/src/components/Ad.jsx
@@ -3,7 +3,7 @@ import React from 'react' ;
 class Ad extends React.Component {
     render () {
         return (
-            <div>
+            <div id="ad">
                 <h2> Click here to keep your identity private!</h2>
                 <img src="https://image.shutterstock.com/image-vector/click-here-button-arrow-pointer-600w-1711547476.jpg" alt="Blue button that says Click Me"></img>
             </div>
