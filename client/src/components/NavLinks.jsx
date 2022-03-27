8d2fa2abd7d6e7895d772d24ab92b2c64d8ee5d0
Cindy Marcelle <88116236+cmarcelle@users.noreply.github.com>
2022-03-27 12:36:51 -0400

    Update NavLinks.jsx
    updated NavLinks component.  no links added, just placeholder text for Rooms 1-4

---
+++ client/src/components/NavLinks.jsx
@@ -0,0 +1,17 @@
+import React from 'react' ;
+
+class NavLinks extends React.Component {
+
+    render ( ) {
+        return (
+            <div>
+               Room 1
+               Room 2
+               Room 3
+               Room 4 
+            </div>
+        );
+    }
+}
+
+export default NavLinks;
