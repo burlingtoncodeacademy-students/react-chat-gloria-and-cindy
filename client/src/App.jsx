5070b7e0eed8ad5d7b99f4482be1b1f7b7a3159e
Gloria Anderson <hamman.gja@gmail.com>
2022-03-27 15:59:02 -0400

    sectioned out components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,34 @@
 //importing hook from React
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-//import Chat Box component
-import ChatBox from "./components/ChatBox.jsx"
+//import components
+import ChatBox from "./components/ChatBox.jsx";
+import NavLinks from "./components/NavLinks.jsx";
+import Ad from "./components/Ad.jsx";
+import Banner from "./components/Banner.jsx";
 
 //style import
-import './styles/App.css';
-import NavLinks from './components/NavLinks.jsx'
-import Ad from './components/Ad.jsx'
-import Banner from './components/Banner.jsx'
+import "./styles/App.css";
 
 export default function App() {
-
-   return (
-    <div className="App">
-      <h1>Hello, Chat!</h1>
-      <NavLinks/>
-      <ChatBox/>
-        </div>
+  return (
+    <main className="App">
+      {/* Banner Component */}
+      <header className="banner">
+        <Banner />
+      </header>
+      {/* NavLinks Component */}
+      <nav className="nav-links">
+        <NavLinks />
+      </nav>
+      {/* ChatBox Component */}
+      <section className="chat-box">
+        <ChatBox />
+      </section>
+      {/* Ad Component */}
+      <aside className="ad">
+        <Ad />
+      </aside>
+    </main>
   );
 }
