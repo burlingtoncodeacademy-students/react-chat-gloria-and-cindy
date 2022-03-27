ea9e866dc49e4d592b83f0f5c16908b2268936ae
Cindy Marcelle <88116236+cmarcelle@users.noreply.github.com>
2022-03-27 12:35:41 -0400

    Update Banner.jsx
    updated banner component with placeholder chat banner clipart

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react' ;
+
+class Banner extends React.Component {
+    render () {
+        return (
+            <div>
+                <h2> Click here to keep your identity private!</h2>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRqtBLzzt0aVQ3N9rESjCX7OqbH7gX0qS2qB84nx-edsyDh2Y&s" alt="Chat banner with five chat bubbles, each a different color.  From left to right the colors are: orange, yellow, green, light blue, and dark blue"></img>
+            </div>
+        );
+    }
+}
+
+export default Banner;
