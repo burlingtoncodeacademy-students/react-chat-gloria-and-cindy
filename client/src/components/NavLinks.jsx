b65ca5a01cb7d5d96a2d96d3c226f3ca55f85a8d
Gloria Anderson <hamman.gja@gmail.com>
2022-03-27 20:07:34 -0400

    note added

--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -1,21 +1,25 @@
 //library imports
-import React from 'react' ;
+import React from "react";
 
 class NavLinks extends React.Component {
+  render() {
+    return (
+      <>
+        <h3>Available Rooms</h3>
 
-    render ( ) {
-        return (
-            <div id="navigation">
-            <h3>Available Rooms</h3>
-               <ul>
-               <li>Room 1</li>
-               <li>Room 2</li>
-               <li>Room 3</li>
-               <li>Room 4</li> 
-               </ul>
-            </div>
-        );
-    }
+        //need to add links/routes to all chat Rooms
+        
+        <div id="navigation">
+          <ul>
+            <li>Room 1</li>
+            <li>Room 2</li>
+            <li>Room 3</li>
+            <li>Room 4</li>
+          </ul>
+        </div>
+      </>
+    );
+  }
 }
 
 export default NavLinks;
