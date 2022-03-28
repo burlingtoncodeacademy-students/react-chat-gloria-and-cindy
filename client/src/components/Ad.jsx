748d09e7d36b81a0c4a5e24174c66c0de2f8bd9d
Gloria Anderson <hamman.gja@gmail.com>
2022-03-27 20:08:13 -0400

    size adjustment and h element to lable

--- client/src/components/Ad.jsx
+++ client/src/components/Ad.jsx
@@ -1,14 +1,24 @@
-import React from 'react' ;
+//library imports
+import React from "react";
+
+//style import
+import "../styles/App.css";
 
 class Ad extends React.Component {
-    render () {
-        return (
-            <div id="ad">
-                <h2> Click here to keep your identity private!</h2>
-                <img src="https://image.shutterstock.com/image-vector/click-here-button-arrow-pointer-600w-1711547476.jpg" alt="Blue button that says Click Me"></img>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div id="ad">
+          <img
+            src="https://image.shutterstock.com/image-vector/click-here-button-arrow-pointer-600w-1711547476.jpg"
+            alt="Blue button that says Click Me"
+            width="300px"
+          ></img>
+          <label> Click here to keep your identity private!</label>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Ad;
